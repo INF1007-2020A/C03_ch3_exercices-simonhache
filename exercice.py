de86241f1a869366ceb4a341157a291280b90380
simonhache <simonhache17@gmail.com>
2020-09-14 18:25:53 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,7 +12,8 @@ def average(a: float, b: float, c: float) -> float:
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     angle_radian = angle_degs/3*math.pi
-    angle_mins_radian = angle_min/3*math.pi
+    angle_mins_radian = angle_mins/3*math.pi
+    angle_secs_
     return 0.0
 
 
