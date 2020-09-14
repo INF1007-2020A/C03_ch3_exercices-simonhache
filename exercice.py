ac8db09e20354db4a3bfcc82bd8c460fda17175a
simonhache <simonhache17@gmail.com>
2020-09-14 18:44:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -11,11 +11,11 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    angle_total = angle_degs + angle_mins_radian + angle_secs_radian
     angle_mins_radian = angle_mins / 60
     angle_secs_radian = angle_secs /(60*60)
+    angle_total = angle_degs + angle_mins_radian + angle_secs_radian
     angle_radian = (angle_degs*math.pi)/180
-    return 
+    return angle_radian 
 
 
 def to_degrees(angle_rads: float) -> tuple:
